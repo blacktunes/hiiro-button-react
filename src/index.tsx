34fdc96bb2a90bbb3c47fc48f7d495c0caf9c0b7
blacktunes <blacktunes@foxmail.com>
2021-02-24 17:14:34 +0800

    style: 提取颜色设置

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './assets/styles/transition.scss'
+import './index.scss'
+import reportWebVitals from './reportWebVitals'
+import Footer from './views/Footer'
 import Header from './views/Header'
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
